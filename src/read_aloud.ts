66488aef835d7fa5b7494865e5f753edd0a2164b
ysnows <guangjin2.xian@zhaopin.com.cn>
2024-04-12 08:16:30 +0800

    tts

--- src/read_aloud.ts
+++ src/read_aloud.ts
@@ -1,11 +1,11 @@
 // Usage
-import { environment, Action, ActionProps, ChatHistory, Clipboard, language, res, uuid } from "@enconvo/api";
-import { speak } from "@/tts/index.js";
+import { Action, ActionProps, ChatHistory, Clipboard, res, uuid } from "@enconvo/api";
+import { speak } from "@/tts/tts.ts";
 
 export default async function main(req: Request) {
     try {
         const { options } = await req.json()
-        const { text, context, tts_providers } = options
+        const { text, context, tts_providers, new_tts_providers } = options
 
         let translateText = text || context || options.stream !== true && await Clipboard.selectedText();
 
@@ -16,9 +16,6 @@ export default async function main(req: Request) {
             await res.context({ id: requestId, role: "human", content: `\n\n> ${displayText}\n\n` })
         }
 
-        const sourceLang = translateText ? await language.detect(translateText) : "en";
-
-
         if (options.stream !== true) {
             await ChatHistory.saveChatMessages({
                 input: translateText,
@@ -27,13 +24,10 @@ export default async function main(req: Request) {
             });
         }
 
-        await speak({ text: translateText, lang: sourceLang, stream: options.stream, streamEnd: options.streamEnd, streamStart: options.streamStart, options: tts_providers });
+        await speak({ text: translateText, stream: options.stream, streamEnd: options.streamEnd, streamStart: options.streamStart, options: new_tts_providers || tts_providers });
 
         const actions: ActionProps[] = [
-            Action.PlayAudio(translateText, "Play Again", false, {
-                commandName: environment.commandName,
-                extensionName: environment.extensionName,
-            }),
+            Action.PlayAudio(translateText, "Play Again", false),
             Action.PauseResumeAudio()
         ]
 
