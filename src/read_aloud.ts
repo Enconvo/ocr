a3919b68e712424de5877826bdaa7e45813134b2
ysnows <guangjin2.xian@zhaopin.com.cn>
2024-04-15 18:12:07 +0800

    link read aloud

--- src/read_aloud.ts
+++ src/read_aloud.ts
@@ -2,44 +2,40 @@
 import { Action, ActionProps, ChatHistory, Clipboard, TTS, res, uuid } from "@enconvo/api";
 
 export default async function main(req: Request) {
-    try {
-        const { options } = await req.json()
-        const { text, context, tts_providers, new_tts_providers } = options
+    const { options } = await req.json()
+    const { text, context, tts_providers, new_tts_providers } = options
 
-        let translateText = text || context || options.stream !== true && await Clipboard.selectedText();
+    let translateText = text || context || options.stream !== true && await Clipboard.selectedText();
 
-        const requestId = uuid()
-        // 如果translateText中有换行符，需要添加> 符号
-        if (translateText) {
-            const displayText = (translateText).replace(/\n/g, "\n> ");
-            await res.context({ id: requestId, role: "human", content: `\n\n> ${displayText}\n\n` })
-        }
-
-        if (options.stream !== true) {
-            await ChatHistory.saveChatMessages({
-                input: translateText,
-                output: translateText,
-                requestId
-            });
-        }
-
-        await TTS.speak({ text: translateText, stream: options.stream, streamEnd: options.streamEnd, ttsOptions: new_tts_providers || tts_providers });
-
-        const actions: ActionProps[] = [
-            Action.PlayAudio({
-                content: translateText, title: "Play Again", closeMainWindow: false
-            }),
-            Action.PauseResumeAudio(),
-            Action.SaveAsAudioFile({ content: translateText })
-        ]
-
-        const output = {
-            content: translateText,
-            actions: actions
-        }
-
-        return output;
-    } catch (err) {
-        return "error" + err;
+    const requestId = uuid()
+    // 如果translateText中有换行符，需要添加> 符号
+    if (translateText) {
+        const displayText = (translateText).replace(/\n/g, "\n> ");
+        await res.context({ id: requestId, role: "human", content: `\n\n> ${displayText}\n\n` })
     }
+
+    if (options.stream !== true) {
+        await ChatHistory.saveChatMessages({
+            input: translateText,
+            output: translateText,
+            requestId
+        });
+    }
+
+    await TTS.speak({ text: translateText, stream: options.stream, streamEnd: options.streamEnd, ttsOptions: new_tts_providers || tts_providers });
+
+    const actions: ActionProps[] = [
+        Action.PlayAudio({
+            content: translateText, title: "Play Again", closeMainWindow: false
+        }),
+        Action.PauseResumeAudio(),
+        Action.SaveAsAudioFile({ content: translateText })
+    ]
+
+    const output = {
+        content: translateText,
+        actions: actions
+    }
+
+    return output;
 }
