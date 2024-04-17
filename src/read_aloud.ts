2fab6bb4c4152a7ce1cd07825c3285cfd4e7dfb4
ysnows <guangjin2.xian@zhaopin.com.cn>
2024-04-17 15:31:58 +0800

    optimize file message

--- src/read_aloud.ts
+++ src/read_aloud.ts
@@ -1,17 +1,39 @@
 // Usage
-import { Action, ActionProps, ChatHistory, Clipboard, TTS, res, uuid } from "@enconvo/api";
+import { Action, ActionProps, ChatHistory, Clipboard, PlayPoolItem, ServiceProvider, TTS, isTextFile, res, uuid } from "@enconvo/api";
 
 export default async function main(req: Request) {
     const { options } = await req.json()
     const { text, context, tts_providers, new_tts_providers } = options
 
-    let translateText = text || context || options.stream !== true && await Clipboard.selectedText();
+    let filePaths: string[] = options.draggedContext || []
+
+    const textFilePaths = filePaths.filter((filePath) => {
+        return isTextFile(filePath)
+    }).map((filePath) => {
+        return {
+            id: uuid(),
+            filePath,
+            type: "file"
+        }
+    })
+    let docContent = null
+    if (textFilePaths.length > 0) {
+        const loader: any = await ServiceProvider.load({
+            extensionName: "chat_with_doc",
+            commandName: "load_docs"
+        })
+        const docs: any[] = await loader.load({ docs: textFilePaths })
+        docContent = docs.map((doc: any) => {
+            return doc.pageContent
+        }).join("\n\n")
+    }
+
+    let translateText = docContent || text || context || options.stream !== true && await Clipboard.selectedText();
 
     const requestId = uuid()
     // 如果translateText中有换行符，需要添加> 符号
     if (translateText) {
-        const displayText = (translateText).replace(/\n/g, "\n> ");
-        await res.context({ id: requestId, role: "human", content: `\n\n> ${displayText}\n\n` })
+        await res.context({ id: requestId, role: "human", content: `${docContent ? 'Read Aloud This Document' : translateText}` })
     }
 
     if (options.stream !== true) {
@@ -22,7 +44,15 @@ export default async function main(req: Request) {
         });
     }
 
-    await TTS.speak({ text: translateText, stream: options.stream, streamEnd: options.streamEnd, ttsOptions: new_tts_providers || tts_providers });
+    await TTS.speak({
+        text: translateText, stream: options.stream, streamEnd: options.streamEnd, ttsOptions: new_tts_providers || tts_providers, playCallBack: async (data: PlayPoolItem) => {
+            console.log("playCallBack", data)
+            await res.write({
+                content: data.text,
+                overwriteText: true,
+            })
+        }
+    });
 
     const actions: ActionProps[] = [
         Action.PlayAudio({
@@ -33,7 +63,7 @@ export default async function main(req: Request) {
     ]
 
     const output = {
-        content: translateText,
+        content: docContent ? "Reading Document " : translateText,
         actions: actions
     }
 
