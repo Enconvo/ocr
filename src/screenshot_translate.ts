902a8a28b48981a954de7f8f7ba5773df09fca04
ysnows <ysnows@163.com>
2025-09-23 20:25:56 +0800

    optimize ocr translate

--- src/screenshot_translate.ts
+++ src/screenshot_translate.ts
@@ -1,9 +1,25 @@
-import { Response, OCRProvider, ScreenshotHelper, SmartBar } from "@enconvo/api";
+import { OCRProvider, ScreenshotHelper, EnconvoResponse, NativeAPI, RequestOptions, Clipboard, showHUD, res, BaseChatMessage, ChatMessageContent } from "@enconvo/api";
 
+interface TranslateRequestParams extends RequestOptions {
+    show_result_in_smartbar: boolean
+    copy_to_clipboard: boolean
+}
 
-export default async function main(req: Request): Promise<Response> {
+export default async function main(req: Request): Promise<EnconvoResponse> {
+    const options: TranslateRequestParams = await req.json()
 
     const { path } = await ScreenshotHelper.selectScreenArea()
+
+    if (options.show_result_in_smartbar) {
+        await res.write({
+            content: BaseChatMessage.user([
+                ChatMessageContent.imageUrl({
+                    url: path
+                })
+            ]),
+            action: res.WriteAction.AppendMessage
+        })
+    }
 
     const ocrProvider = await OCRProvider.fromEnv()
 
@@ -11,8 +27,26 @@ export default async function main(req: Request): Promise<Response> {
         image_url: path,
     })
 
+    console.log('ocrResult', ocrResult)
 
-    await SmartBar.show({ hotKey: "translate|translate|execute", context: ocrResult.text })
+    const translateResult = await NativeAPI.callCommand("translate|translate", { input_text: ocrResult.text })
+    const contents = translateResult?.data?.content || []
 
-    return Response.none()
+    if (Array.isArray(contents) && contents.length > 0) {
+        const content = contents[0]
+        const text = content?.text || ''
+
+        console.log('translateResult', text)
+        if (options.copy_to_clipboard) {
+            await Clipboard.copy(text)
+            await showHUD("Translated results copied to clipboard")
+        }
+
+        if (options.show_result_in_smartbar) {
+            return text
+        }
+    }
+
+
+    return EnconvoResponse.none()
 }
